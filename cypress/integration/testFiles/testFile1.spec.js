b63f510f49ca006c6a364fad199514a28e417e30
Artem Nikolaev <1980.1@mail.ru>
2020-10-24 00:00:33 +0300

    add tests

--- cypress/integration/testFiles/testFile1.spec.js
+++ cypress/integration/testFiles/testFile1.spec.js
@@ -10,23 +10,30 @@ describe('test', () => {
     });
 
     it('should login', () => {
-        cy.get('#normal_login_email').type(testFile.email);
-        cy.get('#normal_login_password').type(testFile.password);
+        cy.get('#normal_login_email').type(testFile[0].email);
+        cy.get('#normal_login_password').type(testFile[0].password);
         cy.get('[type="submit"]').click();
         cy.get('.ant-btn-link').click();
         cy.wait(2000)
-        //cy.get('h1').should('have.text', testFile.userName);         //elem.getText()
-
-        //2//cy.get("h1").invoke('text').then((text) => {              //elem.getText()
-            //expect(text).equal(testFile.userName);
-       // });
-
-        cy.get("h1").then(function($h1) {   //elem.getText()
-            expect($h1.text()).equal(testFile.userName)
+        cy.get("h1").then(function($h1) {
+            expect($h1.text()).equal(testFile[0].userName)
             cy.log($h1.text())
         })
     });
 });
+
+//Варианты - elem.getText()
+
+//1//cy.get('h1').should('have.text', testFile.userName);
+
+//2//cy.get("h1").invoke('text').then((text) => {
+//expect(text).equal(testFile.userName);
+// });
+
+//3// cy.get("h1").then(function($h1) {
+//             expect($h1.text()).equal(testFile[0].userName)
+//             cy.log($h1.text())
+//         })
 
 
 
--- cypress/integration/testFiles/testFile2.spec.js
+++ cypress/integration/testFiles/testFile2.spec.js
@@ -1,4 +1,5 @@
 import cypress  from "../../../cypress.json";
+import testFile from "../../fixtures/testFile.json";
 
 describe('DEMO QA TESTS', () => {
     before('open demo qa', () => {
@@ -6,6 +7,13 @@ describe('DEMO QA TESTS', () => {
     })
 
     it('to go elements', () => {
-        cy.get('.card.mt-4.top-card:nth-child(1)').click();
+        cy.get('.card.mt-4.top-card:nth-child(1)').click()
+        .url().should('eq', testFile[1].urlPageElements);
     })
+
+    it('should text on the page "Please select an item from left to start practice."', () => {
+        cy.get('.col-md-6').should('have.text',testFile[1].elementsText);
+    })
+
+
 })
